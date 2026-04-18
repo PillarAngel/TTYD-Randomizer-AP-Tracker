957419bf04b55edc715aa5342c850859247ef36f
JRTRCT <joshrthomson@gmail.com>
2026-04-18 10:39:02 -0400

    Test fix.

--- scripts/logic/logic.lua
+++ scripts/logic/logic.lua
@@ -1,8 +1,11 @@
 STARS = {"DiamondStar", "EmeraldStar", "GoldStar", "RubyStar", "SapphireStar", "GarnetStar", "CrystalStar"}
-STAR_LOCATIONS = {"@Pit of 100 Trials/Hooktail's Castle Hooktail's Room - Diamond Star", "@Pit of 100 Trials/Great Tree Entrance - Emerald Star",
-"@Pit of 100 Trials/Glitzville Arena - Gold Star", "@Pit of 100 Trials/Creepy Steeple Upper Room - Ruby Star",
-"@Pit of 100 Trials/Pirate's Grotto Cortez' Hoard - Sapphire Star", "@Pit of 100 Trials/Poshley Heights Sanctum Altar - Garnet Star",
-"@Pit of 100 Trials/X-Naut Fortress Boss Room - Crystal Star"}
+STAR_LOCATIONS = {"@Magical Map/Pit of 100 Trials/Hooktail's Castle Hooktail's Room - Diamond Star",
+"@Magical Map/Pit of 100 Trials/Great Tree Entrance - Emerald Star",
+"@Magical Map/Pit of 100 Trials/Glitzville Arena - Gold Star",
+"@Magical Map/Pit of 100 Trials/Creepy Steeple Upper Room - Ruby Star",
+"@Magical Map/Pit of 100 Trials/Pirate's Grotto Cortez' Hoard - Sapphire Star",
+"@Magical Map/Pit of 100 Trials/Poshley Heights Sanctum Altar - Garnet Star",
+"@Magical Map/Pit of 100 Trials/X-Naut Fortress Boss Room - Crystal Star"}
 
 function stars(AMOUNT)
 	AMOUNT = tonumber(AMOUNT)
